9251990c5d364ed18c4d887fe1fbfd49b0ecec8b
Endoplex <76065821+Endoplex@users.noreply.github.com>
2022-11-14 22:47:38 -0600

    push

--- csc4243/src/screens/home.js
+++ csc4243/src/screens/home.js
@@ -3,6 +3,11 @@ const React = require('react');
 class Home extends React.Component {
     constructor(props) {
         super(props)
-        this.props
+        this.props.name
+    }
+    render() {
+        <title>Account</title>,
+        <h1>Create New Resume</h1>
+        <
     }
 }
