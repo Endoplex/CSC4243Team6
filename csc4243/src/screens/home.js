67096cdaf47bf9d7b3237b04890f8e5258656b96
Endoplex <76065821+Endoplex@users.noreply.github.com>
2022-11-14 21:46:55 -0600

    Push

--- csc4243/src/screens/home.js
+++ csc4243/src/screens/home.js
@@ -1,5 +1,17 @@
+<<<<<<< HEAD
+const React = require('react');
+=======
 import { NavigationType, useNavigate } from "react-router-dom";
+>>>>>>> 5fe970c13f03b533199f46590f4fae5f7f8d02ab
 
+<<<<<<< HEAD
+class Home extends React.Component {
+    constructor(props) {
+        super(props)
+        this.props
+    }
+}
+=======
 const Page = () => {
     let navigate = useNavigate();
 
@@ -9,3 +21,4 @@ const Page = () => {
 }
 
 export default Page;
+>>>>>>> 5fe970c13f03b533199f46590f4fae5f7f8d02ab
