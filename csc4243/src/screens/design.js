032553fe5b18f316264348c8afdb663635f12a16
hreine2 <57732323+hreine2@users.noreply.github.com>
2022-11-15 00:32:24 -0600

    Designs added
    designs added, not clickable yet

--- csc4243/src/screens/design.js
+++ csc4243/src/screens/design.js
@@ -1,10 +1,25 @@
 import { NavigationType, useNavigate } from "react-router-dom";
+import basic from '../images/template_basic.png';
+import professional from '../images/template_professional.png';
+import green from '../images/template_green.png';
+import silicon from '../images/template_silicon.png';
+import '../components/css/design.css';
 
 const Page = () => {
     let navigate = useNavigate();
 
     return (
-        <button onClick={() => {{(navigate('/result'))}}}>Result</button>
+        <div className="Design">
+            <div className="Header">
+                <h1>Design</h1>
+            </div>
+            <img className="style" src={basic} alt="" />
+            <img className="style" src={professional} alt="" />
+            <img className="style" src={green} alt="" />
+            <img className="style" src={silicon} alt="" />
+            <br />
+            <button onClick={() => {{(navigate('/result'))}}}>Result</button>
+        </div>
     );
 }
 
