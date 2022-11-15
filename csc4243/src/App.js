a56b1569cbb7cd914579b7f7c1fcb178380ec8c5
JDIK543 <64238989+JDIK543@users.noreply.github.com>
2022-11-14 20:59:42 -0600

    Added screens

--- csc4243/src/App.js
+++ csc4243/src/App.js
@@ -1,5 +1,15 @@
+import React, { } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import HomeScreen from './screens/home.js';
+import EducationScreen from './screens/education.js';
+import ExperienceScreen from './screens/experience.js';
+import DesignScreen from './screens/design.js';
+import PersonalScreen from './screens/personal.js';
+import ResultScreen from './screens/result.js';
 
 function App() {
   return (
@@ -7,7 +17,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> hello
         </p>
         <a
           className="App-link"
