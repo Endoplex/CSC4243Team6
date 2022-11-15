ed18ce34b057369dc268a5ff0ea3fefc63133c7d
Endoplex <76065821+Endoplex@users.noreply.github.com>
2022-11-14 21:47:32 -0600

    sync

--- csc4243/src/screens/home.js
+++ csc4243/src/screens/home.js
@@ -1,24 +1,8 @@
-<<<<<<< HEAD
 const React = require('react');
-=======
-import { NavigationType, useNavigate } from "react-router-dom";
->>>>>>> 5fe970c13f03b533199f46590f4fae5f7f8d02ab
 
-<<<<<<< HEAD
 class Home extends React.Component {
     constructor(props) {
         super(props)
         this.props
     }
 }
-=======
-const Page = () => {
-    let navigate = useNavigate();
-
-    return (
-        <button onClick={() => {{(navigate('/personal'))}}}>Personal</button>
-    );
-}
-
-export default Page;
->>>>>>> 5fe970c13f03b533199f46590f4fae5f7f8d02ab
