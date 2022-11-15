9ec8a30f5c29b542e438e60ab7d5b23bdcc3f771
Bruce Quach <quachbruce@gmail.com>
2022-11-14 23:19:16 -0600

    Complete home page

--- csc4243/src/screens/home.js
+++ csc4243/src/screens/home.js
@@ -1,18 +1,25 @@
-const React = require('react');
+import { NavigationType, useNavigate } from "react-router-dom";
+import "../components/css/home.css";
 
-class Home extends React.Component {
-    constructor(props) {
-        super(props)
-        this.props.name
-    }
-    render() {
-        <title>Account</title>,
-<<<<<<< HEAD
-        <h1>Create New Resume</h1>,
-        <h1></h1>
-=======
-        <h1>Create New Resume</h1>,
-        <h2></h2>
->>>>>>> a205bfb18a628c38dcc4baa75009f93f42749510
-    }
+const Page = () => {
+    let navigate = useNavigate();
+
+    return (
+        <div class="home">
+            <h1>Before we get started, what industry does your major classify as?</h1>
+            <select name="industry" id="industry">
+                <option value="" disabled selected>Select Your Option</option>
+                <option value="engineering">Engineering</option>
+                <option value="sciences">Physical Sciences</option>
+                <option value="sciences">Life Sciences</option>
+                <option value="humanities">Humanities</option>
+                <option value="social">Social Sciences</option>
+                <option value="social">Business</option>
+            </select>
+            <h1>CREATE YOUR RESUME HERE</h1>
+            <button onClick={() => navigate('/personal')}>Start Resume</button>
+        </div>
+    );
 }
+
+export default Page;
