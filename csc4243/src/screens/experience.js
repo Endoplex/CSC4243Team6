40e68eab20dfafe35d3408a519c681448006557e
Toriyuki <51471099+Toriyuki@users.noreply.github.com>
2022-12-08 01:33:22 -0600

    Experience Updated

--- csc4243/src/screens/experience.js
+++ csc4243/src/screens/experience.js
@@ -5,6 +5,8 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import FormControl from '@mui/material/FormControl';
 import { FaQuestionCircle, FaArrowLeft } from "react-icons/fa";
+import "../components/css/experience.css";
+import logo from '../images/logo.png';
 
 const expDefaultValues = {
     experience1: '',
@@ -26,6 +28,12 @@ const expDefaultValues = {
 const Page = () => {
 
     const [values, setValues] = React.useState(expDefaultValues);
+
+    const [count, setCount] = React.useState(0);
+
+    const increase = () => {
+        setCount(count => count + 1);
+      };
 
     const handleChange = (e) => {
         const {name, value} = e.target;
@@ -70,13 +78,143 @@ const Page = () => {
         popup.classList.toggle("show");
     }
 
-    return (
-        <div className="Experience">
-            <div className="Header">
-                <div class="left-arrow" onClick={() => navigate('/education')}> <FaArrowLeft /> </div>
-                <div className="Logo"><h1>Online Resume Builder</h1></div>
-                <div className="Header_text"><h1>Experiences</h1></div>
+    let addbutton;
+    let extraExper1;
+    let extraExper2;
+
+    if(count >= 2) {
+        extraExper1 = <div className='selection_box'>
+        <Box sx={{ align: 'center' }}>
+        <FormControl sx={{ m: 3, width: '75ch' }} variant="standard">
+            <div>
+                <TextField
+                    id="exp2"
+                    name="experience2"
+                    label="Experience 2"
+                    values={values.experience2}
+                    onChange={handleChange}
+                    variant="standard"
+                />
+                <div class="popup" onClick={() => exp2Help()}><FaQuestionCircle />
+                    <span class="popuptext" id="exp2Popup">For the second experience, this could be either the next most interesting, or the next most recent</span>
+                </div>
             </div>
+            <TextField
+                id="pos2"
+                name="position2"
+                label="Position/Title if applicable"
+                values={values.position2}
+                onChange={handleChange}
+                variant="standard"
+            />
+            <TextField
+                id="start2"
+                name="start2"
+                label="Start Ex: Jan 2022"
+                values={values.start2}
+                onChange={handleChange}
+                variant="standard"
+            />
+            <TextField
+                id="end2"
+                name="end2"
+                label="End Ex: Dec 2022 or Present"
+                values={values.end2}
+                onChange={handleChange}
+                variant="standard"
+            />
+            <div>
+                <TextField
+                    id="description2"
+                    name="description2"
+                    label="Description"
+                    values={values.description2}
+                    multiline
+                    onChange={handleChange}
+                    variant="standard"
+                />
+                <div class="popup" onClick={() => desc2Help()}><FaQuestionCircle />
+                    <span class="popuptext" id="desc2Popup">Here, you should go into detail about the experience. Don't be afraid to brag if you think a particular aspect of the experience was interesting</span>
+                </div>
+            </div>
+        </FormControl>
+        </Box>
+    </div>
+    }
+
+    if(count >= 3) {
+        extraExper2 = <div className='selection_box'>
+        <Box sx={{ align: 'center'}}>
+        <FormControl sx={{ m: 3, width: '75ch' }} variant="standard">
+            <div>
+                <TextField
+                    id="exp3"
+                    name="experience3"
+                    label="Experience 3"
+                    values={values.experience3}
+                    onChange={handleChange}
+                    variant="standard"
+                />
+                <div class="popup" onClick={() => exp3Help()}><FaQuestionCircle />
+                    <span class="popuptext" id="exp3Popup">Make sure to stay in chronological order; if you have any more experiences, show them off!</span>
+                </div>
+            </div>
+            <TextField
+                id="pos3"
+                name="position3"
+                label="Position/Title if applicable"
+                values={values.position3}
+                onChange={handleChange}
+                variant="standard"
+            />
+            <TextField
+                id="start3"
+                name="start3"
+                label="Start Ex: Jan 2022"
+                values={values.start3}
+                onChange={handleChange}
+                variant="standard"
+            />
+            <TextField
+                id="end3"
+                name="end3"
+                label="End Ex: Dec 2022 or Present"
+                values={values.end3}
+                onChange={handleChange}
+                variant="standard"
+            />
+            <div>
+                <TextField
+                    id="description3"
+                    name="description3"
+                    label="Description"
+                    values={values.description3}
+                    multiline
+                    onChange={handleChange}
+                    variant="standard"
+                />
+                <div class="popup" onClick={() => desc3Help()}><FaQuestionCircle />
+                    <span class="popuptext" id="desc3Popup">Here, you should go into detail about the experience. Don't be afraid to brag if you think a particular aspect of the experience was interesting</span>
+                </div>
+            </div>
+        </FormControl>
+        </Box>
+    </div>
+    }
+
+    if(count < 3) {
+        addbutton = <button className="control__btn" onClick={increase}>+</button>
+    }
+
+
+    return (
+        <div className="experience">
+            <div className="header">
+                <img className='logo' src={logo} alt=""></img>
+                <div className="header_text"><h1>Experience</h1></div>
+            </div>
+
+            <div><h1></h1></div>
             {localStorage.getItem('experienceKey', JSON.stringify) == '{"industry":"engineering"}' && <div><strong>This is where you input past work experience. Since you selected the Engineering discipline, we suggest adding projects or research you participated in as well!</strong></div>}
             {localStorage.getItem('experienceKey', JSON.stringify) == '{"industry":"physSciences"}' && <div><strong>This is where you input past work experience. Since you selected the Physical Sciences discipline, we suggest adding lab experiences or research you participated in as well!</strong></div>}
             {localStorage.getItem('experienceKey', JSON.stringify) == '{"industry":"lifeSciences"}' && <div><strong>This is where you input past work experience. Since you selected the Life Sciences discipline, we suggest adding lab research or paper publications you participated in as well!</strong></div>}
@@ -141,121 +279,14 @@ const Page = () => {
             </Box>      
         </div>
         <div><h1></h1></div>
-        <div className='selection_box'>
-            <Box sx={{ align: 'center' }}>
-            <FormControl sx={{ m: 3, width: '75ch' }} variant="standard">
-                <div>
-                    <TextField
-                        id="exp2"
-                        name="experience2"
-                        label="Experience 2"
-                        values={values.experience2}
-                        onChange={handleChange}
-                        variant="standard"
-                    />
-                    <div class="popup" onClick={() => exp2Help()}><FaQuestionCircle />
-                        <span class="popuptext" id="exp2Popup">For the second experience, this could be either the next most interesting, or the next most recent</span>
-                    </div>
-                </div>
-                <TextField
-                    id="pos2"
-                    name="position2"
-                    label="Position/Title if applicable"
-                    values={values.position2}
-                    onChange={handleChange}
-                    variant="standard"
-                />
-                <TextField
-                    id="start2"
-                    name="start2"
-                    label="Start Ex: Jan 2022"
-                    values={values.start2}
-                    onChange={handleChange}
-                    variant="standard"
-                />
-                <TextField
-                    id="end2"
-                    name="end2"
-                    label="End Ex: Dec 2022 or Present"
-                    values={values.end2}
-                    onChange={handleChange}
-                    variant="standard"
-                />
-                <div>
-                    <TextField
-                        id="description2"
-                        name="description2"
-                        label="Description"
-                        values={values.description2}
-                        multiline
-                        onChange={handleChange}
-                        variant="standard"
-                    />
-                    <div class="popup" onClick={() => desc2Help()}><FaQuestionCircle />
-                        <span class="popuptext" id="desc2Popup">Here, you should go into detail about the experience. Don't be afraid to brag if you think a particular aspect of the experience was interesting</span>
-                    </div>
-                </div>
-            </FormControl>
-            </Box>
-        </div>
+        
+        {extraExper1}
         <div><h1></h1></div>
-        <div className='selection_box'>
-            <Box sx={{ align: 'center'}}>
-            <FormControl sx={{ m: 3, width: '75ch' }} variant="standard">
-                <div>
-                    <TextField
-                        id="exp3"
-                        name="experience3"
-                        label="Experience 3"
-                        values={values.experience3}
-                        onChange={handleChange}
-                        variant="standard"
-                    />
-                    <div class="popup" onClick={() => exp3Help()}><FaQuestionCircle />
-                        <span class="popuptext" id="exp3Popup">Make sure to stay in chronological order; if you have any more experiences, show them off!</span>
-                    </div>
-                </div>
-                <TextField
-                    id="pos3"
-                    name="position3"
-                    label="Position/Title if applicable"
-                    values={values.position3}
-                    onChange={handleChange}
-                    variant="standard"
-                />
-                <TextField
-                    id="start3"
-                    name="start3"
-                    label="Start Ex: Jan 2022"
-                    values={values.start3}
-                    onChange={handleChange}
-                    variant="standard"
-                />
-                <TextField
-                    id="end3"
-                    name="end3"
-                    label="End Ex: Dec 2022 or Present"
-                    values={values.end3}
-                    onChange={handleChange}
-                    variant="standard"
-                />
-                <div>
-                    <TextField
-                        id="description3"
-                        name="description3"
-                        label="Description"
-                        values={values.description3}
-                        multiline
-                        onChange={handleChange}
-                        variant="standard"
-                    />
-                    <div class="popup" onClick={() => desc3Help()}><FaQuestionCircle />
-                        <span class="popuptext" id="desc3Popup">Here, you should go into detail about the experience. Don't be afraid to brag if you think a particular aspect of the experience was interesting</span>
-                    </div>
-                </div>
-            </FormControl>
-            </Box>
-        </div>
+
+        {extraExper2}
+        <div><h1></h1></div>
+
+        <div>{addbutton}</div>
         <div><h1></h1></div>
         <button style={{ width: "175px", height: "60px" }} onClick={() => {{(navigate('/design'))}}}>Design</button>
             {/* <button style={{ width: "175px", height: "60px" }} onClick={console.log(values)}>Test info</button> */}
